271cdbae04d78d89a5561a4a712d6814e4d29adc
AK0728 <120946981+AK0728@users.noreply.github.com>
2025-05-02 11:01:44 +0530

    add initial files

---
+++ CounterTest.java
@@ -0,0 +1,22 @@
+package p5;
+class Counter {
+static int count = 0;
+
+public Counter() {
+count++;
+}
+
+public static void displayCount() {
+System.out.println("Count:" + count);
+}
+}
+public class CounterTest {
+
+	public static void main(String[] args) {
+		new Counter();
+		new Counter();
+		Counter.displayCount();
+
+	}
+
+}
